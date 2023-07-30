cfbd4883ff8bcc1eaddfc1d1a3d241fbc98b4b20
Seong <seonghyunryoo@gmail.com>
2023-07-30 14:35:43 -0400

    Quiet clippy

--- src/app/model/game-state.service.ts
+++ src/app/model/game-state.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { GameStates } from './game-states';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
