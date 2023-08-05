643ae5ef0aee2b192c15a7dba49014741918ea75
Seong <seonghyunryoo@gmail.com>
2023-08-04 23:32:13 -0400

    layout styling

--- src/app/model/event-service/event.service.ts
+++ src/app/model/event-service/event.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
 
+interface CallbackData {
+  listenerId: string;
+  callback: Function;
+}
+
+export interface EventsStore {
+  [eventId: string]: CallbackData[];
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EventService {
-
-  constructor() { }
+  events: EventsStore = {};
+  constructor() {}
+  subscribe(eventId: string, listenerId: string, callback: Function) {
+    if (!this.events[eventId]) {
+      this.events[eventId] = [];
+    }
+    const callbackData: CallbackData = {
+      listenerId,
+      callback,
+    };
+    this.events[eventId].push(callbackData);
+  }
+  trigger(eventId: string) {
+    if (this.events[eventId]) {
+      for (const callbackData of this.events[eventId]) {
+        callbackData.callback();
+      }
+    }
+  }
+  unsubscribe(eventId: string, listenerId: string) {
+    if (this.events[eventId]) {
+      const filteredCallbacks = this.events[eventId].filter(
+        (data: CallbackData) => data.listenerId === listenerId
+      );
+      this.events[eventId] = [...filteredCallbacks];
+    }
+  }
 }
--- src/app/view/game-view/play-view/battlefield/hero-profile/hero-profile.component.ts
+++ src/app/view/game-view/play-view/battlefield/hero-profile/hero-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { EventService } from 'src/app/model/event-service/event.service';
 import { Hero } from 'src/app/model/gameplay-model/heroes/hero';
 
 @Component({
@@ -6,24 +7,53 @@ import { Hero } from 'src/app/model/gameplay-model/heroes/hero';
   templateUrl: './hero-profile.component.html',
   styleUrls: ['./hero-profile.component.scss'],
 })
-export class HeroProfileComponent {
+export class HeroProfileComponent implements OnInit, OnDestroy {
   @Input({ required: true })
   hero!: Hero;
   isOpen: boolean = false;
+  listenerId: string = '';
+  static CLOSE_HERO_ACCORDIONS: string = 'close-hero-accordions';
 
-  constructor() {}
+  eventService: EventService;
+
+  constructor(eventService: EventService) {
+    this.eventService = eventService;
+  }
+
+  closeAccordion() {
+    this.isOpen = false;
+  }
+
+  ngOnInit() {
+    this.listenerId = this.hero.id;
+    this.eventService.subscribe(
+      HeroProfileComponent.CLOSE_HERO_ACCORDIONS,
+      this.hero.id,
+      () => this.closeAccordion()
+    );
+  }
+
+  ngOnDestroy() {
+    this.eventService.unsubscribe(
+      HeroProfileComponent.CLOSE_HERO_ACCORDIONS,
+      this.listenerId
+    );
+  }
 
   toggleAccordion() {
+    if (!this.isOpen) {
+      this.eventService.trigger(HeroProfileComponent.CLOSE_HERO_ACCORDIONS);
+    }
     this.isOpen = !this.isOpen;
   }
 
   getAccordionId() {
-    return this.hero.id + "-hero-id"
+    return this.hero.id + '-hero-id';
   }
 
   getAccordionClass() {
-    return "hero-info" + (this.isOpen
-      ? " hero-info--opened"
-      : " hero-info--closed");
+    return (
+      'hero-info' + (this.isOpen ? ' hero-info--opened' : ' hero-info--closed')
+    );
   }
 }
